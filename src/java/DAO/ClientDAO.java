10f7e6eddec6c68c601c67e4b7f46215cf9ab48d
Evegenn <demerbrise@gmail.com>
2016-04-25 00:45:54 +0300

    write two methods for getting list of users card by using: JPA EJB scope - request

--- src/java/DAO/ClientDAO.java
+++ src/java/DAO/ClientDAO.java
@@ -5,10 +5,55 @@
  */
 package DAO;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import model.Client;
+import model.CreditCards;
+
 /**
  *
  * @author Evegen
  */
+@Stateless
 public class ClientDAO {
     
+    @PersistenceContext
+    private EntityManager em;
+    
+    /**
+     * This method return client by using EntityManager
+     * @param email
+     * @return 
+     */
+    public Client getClientByEmail(String email) {
+        Client client = new Client();
+        System.out.println("user+++" + email);
+        List<Client> resultList = em.createNamedQuery("Client.findByEmail")
+                .setParameter("email", email)
+                .getResultList();
+        if (resultList.size()>0) {
+            client = resultList.get(0);
+            return client;
+        }
+        else {
+          return null;  
+        }
+    }
+    
+    /**
+     * 
+     * @param client_id
+     * @return 
+     */
+    public List<CreditCards> getAllCardsByClientID(int client_id) {
+        Client find = em.find(Client.class, client_id);
+        
+        List<CreditCards>  list = find.getCreditCardsList();
+        
+        return list;
+    }
 }
