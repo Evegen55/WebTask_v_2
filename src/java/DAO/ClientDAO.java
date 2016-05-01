d073e33b8f2361085ed41f755097902afbb153f2
Evegenn <demerbrise@gmail.com>
2016-05-01 13:13:27 +0300

    write EJB logic for getting list of all users bank accounts

--- src/java/DAO/ClientDAO.java
+++ src/java/DAO/ClientDAO.java
@@ -9,6 +9,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import model.BankAccount;
 import model.Client;
 import model.CreditCards;
 
@@ -46,5 +47,15 @@ public class ClientDAO {
     public List<CreditCards> getAllCardsByClientID(int client_id) {
         Client find = em.find(Client.class, client_id);
         return find.getCreditCardsList();
+    }
+
+    /**
+     * 
+     * @param client_id
+     * @return 
+     */
+    public List<BankAccount> getAllAccountsByClientID(int client_id) {
+        Client find = em.find(Client.class, client_id);
+        return find.getBankAccountList();
     }
 }
