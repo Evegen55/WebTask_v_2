9c3a681e5dd01a77200bdaf608d4e4250d1de3df
Evegenn <demerbrise@gmail.com>
2016-05-01 14:10:35 +0300

    create a stateless EJB component that using DAO GOF-pattern for getting clients payments history

---
+++ src/java/DAO/PaymentsHistDAO.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 Evegen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package DAO;
+
+import java.util.List;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Evegen
+ */
+@Stateless
+public class PaymentsHistDAO {
+
+    public List getAllPaymetnsHistoryByClientID(int client_id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+    public List getAllPaymetnsHistoryByBeneficiarID(int client_id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
