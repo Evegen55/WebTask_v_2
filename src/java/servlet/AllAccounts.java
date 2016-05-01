9259f3dc8077538a4a051f7b564715c8752eea55
Evegenn <demerbrise@gmail.com>
2016-05-01 13:13:10 +0300

    write servlet's logic for getting list of all users bank accounts

--- src/java/servlet/AllAccounts.java
+++ src/java/servlet/AllAccounts.java
@@ -15,13 +15,18 @@
  */
 package servlet;
 
+import DAO.ClientDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.BankAccount;
+import model.Client;
 
 /**
  *
@@ -30,6 +35,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "AllAccounts", urlPatterns = {"/AllAccounts"})
 public class AllAccounts extends HttpServlet {
 
+    //@EJB
+    @Inject
+    private ClientDAO clientDAO;
+    
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -42,18 +51,11 @@ public class AllAccounts extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AllAccounts</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AllAccounts at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        Client client = clientDAO.getClientByEmail(request.getRemoteUser());
+        List<BankAccount> list = clientDAO.getAllAccountsByClientID(client.getClientID());
+        request.setAttribute("allaccounts", list);
+        request.getRequestDispatcher("/simple_user_pages/allaccounts.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
