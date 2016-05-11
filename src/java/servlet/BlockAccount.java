4da99427dbd52c1f65fcd06234b7ef88c3be4fd3
Evegenn <demerbrise@gmail.com>
2016-05-12 01:52:00 +0300

    rewrite servlet

--- src/java/servlet/BlockAccount.java
+++ src/java/servlet/BlockAccount.java
@@ -70,7 +70,7 @@ public class BlockAccount extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        doPost(request, response);
     }
 
     /**
