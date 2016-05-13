c3a02d79c01e9b2ca534024fc0da4904925b7db7
Evegenn <demerbrise@gmail.com>
2016-05-14 02:52:10 +0300

    rewrite doGet method for invoke doPost

--- src/java/servlet/MakePayment.java
+++ src/java/servlet/MakePayment.java
@@ -117,7 +117,7 @@ public class MakePayment extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        doPost(request, response);
     }
 
     /**
